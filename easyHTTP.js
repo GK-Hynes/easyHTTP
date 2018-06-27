f5dce0adc7634fe003c03dbd602f36e60e23b499
Gerard Hynes <gkhynes@gmail.com>
2018-06-27 22:04:51 +0100

    Initial commit

---
+++ easyHTTP.js
@@ -0,0 +1,23 @@
+function easyHTTP() {
+  this.http = new XMLHttpRequest();
+}
+
+// GET Requests
+easyHTTP.prototype.get = function(url, callback) {
+  this.http.open("GET", url, true);
+
+  let self = this;
+  this.http.onload = function() {
+    if (self.http.status === 200) {
+      callback(null, self.http.responseText);
+    } else {
+      callback("Error: " + self.http.status);
+    }
+  };
+
+  this.http.send();
+};
+
+// POST Requests
+// PUT Requests
+// DELETE Requests
