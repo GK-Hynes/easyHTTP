48cc1a8cc55fe5efc5a8a2836289130d9917c328
Gerard Hynes <gkhynes@gmail.com>
2018-06-28 21:49:45 +0100

    Add post, put, and delete routes

--- easyHTTP.js
+++ easyHTTP.js
@@ -3,13 +3,55 @@ function easyHTTP() {
 }
 
 // GET Requests
-easyHTTP.prototype.get = function(url, callback) {
-  this.http.open("GET", url, true);
+// easyHTTP.prototype.get = function(url, callback) {
+//   this.http.open("GET", url, true);
+
+//   let self = this;
+//   this.http.onload = function() {
+//     if (self.http.status === 200) {
+//       callback(null, self.http.responseText);
+//     } else {
+//       callback("Error: " + self.http.status);
+//     }
+//   };
+
+//   this.http.send();
+// };
+
+// POST Requests
+easyHTTP.prototype.post = function(url, data, callback) {
+  this.http.open("POST", url, true);
+  this.http.setRequestHeader("Content-type", "application/json");
+
+  let self = this;
+  this.http.onload = function() {
+    callback(null, self.http.responseText);
+  };
+
+  this.http.send(JSON.stringify(data));
+};
+
+// PUT Requests
+easyHTTP.prototype.put = function(url, data, callback) {
+  this.http.open("PUT", url, true);
+  this.http.setRequestHeader("Content-type", "application/json");
+
+  let self = this;
+  this.http.onload = function() {
+    callback(null, self.http.responseText);
+  };
+
+  this.http.send(JSON.stringify(data));
+};
+
+// DELETE Requests
+easyHTTP.prototype.delete = function(url, callback) {
+  this.http.open("DELETE", url, true);
 
   let self = this;
   this.http.onload = function() {
     if (self.http.status === 200) {
-      callback(null, self.http.responseText);
+      callback(null, "Post Deleted");
     } else {
       callback("Error: " + self.http.status);
     }
@@ -17,7 +59,3 @@ easyHTTP.prototype.get = function(url, callback) {
 
   this.http.send();
 };
-
-// POST Requests
-// PUT Requests
-// DELETE Requests
